28da33839944b1d97fdb9b21f9d56af658c1e170
developerthierry <developerthierry@gmail.com>
2025-11-21 18:53:24 +0000

    Map Ingredients List, Array State

--- Main.jsx
+++ Main.jsx
@@ -1,12 +1,32 @@
-export default function Main(){
-    return(<main>
-        <form className="add-ingredient-form">
-            <input
-            type="text"
-            placeholder="e.g. oregano"
-            aria-label="Add ingredient"
-            />
-            <button>Add Ingredient</button>
-        </form>
-    </main>)
+import { useState } from "react";
+export default function Main() {
+  const [ingredients, setIngredients] = useState([
+    "Chicken",
+    "Turkey",
+    "Oregano",
+  ]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+  return (
+    <main>
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add Ingredient</button>
+      </form>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
